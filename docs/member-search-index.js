e1035b5080aa081a935e3013ae730dcc6141dc23
Batmansanty49 <143793606+Batmansanty49@users.noreply.github.com>
2023-10-12 22:25:59 -0600

    Adición de javadoc

---
+++ docs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"poop3_agenda","c":"POOP3_Agenda","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"poop3_agenda","c":"POOP3_Agenda","l":"POOP3_Agenda()","u":"%3Cinit%3E()"}];updateSearchResults();
---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"poop3_agenda","l":"POOP3_Agenda"}];updateSearchResults();
